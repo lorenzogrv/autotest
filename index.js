135af3e69bb5472e6415eab21adccf6394390d4c
Lorenzo García Rivera <lorenzogrv@gmail.com>
2015-05-06 22:12:44 +0200

    reserve oop namespace of iai-oop

---
+++ index.js
@@ -0,0 +1,12 @@
+var oop = require('iai-oop');
+
+//
+// This file is the main entry point.
+// Define here only the namespace contents
+//
+
+var exports = oop( exports );
+
+// the oop namespace is reserved (not writable) for the OOP standard API
+exports.visible('oop', oop);
+
