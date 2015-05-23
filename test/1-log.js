516113947223dcbd7b5e8dcdb57108b6990930a3
Lorenzo García Rivera <lorenzogrv@gmail.com>
2015-05-23 11:26:40 +0200

    fix stdout & stderr pollution on tests

--- test/1-log.js
+++ test/1-log.js
@@ -1,27 +1,32 @@
 var assert = require('assert');
 var iai = require('..');
 
-var log = iai.log;
-assert.equal( log.filename, __filename );
+module.exports = TEST;
+require.main === module && TEST();
 
-var methods = [
-  'fatal', 'error', 'warn', 'info', 'verb', // log levels
-  'out', 'err', // write on output streams
-  'msg' // message formatter
-];
-for( name in methods ){
-  name = methods[name];
-  assert.equal(
-    typeof log[name], 'function',
-    'log#'+name+' should be a function'
-  );
-}
+function TEST(){
+  var log = iai.log;
+  assert.equal( log.filename, __filename );
 
-// FINISHING
-var sources = require('../api/sources')( module )
-try {
-  assert.equal( sources.length, 6 );
-} catch( error ){
-  console.error('WARN: This test should require 4 source files exactly');
-  console.error( 'source files required:\n  %s', sources.join('\n  ') );
+  var methods = [
+    'fatal', 'error', 'warn', 'info', 'verb', // log levels
+    'out', 'err', // write on output streams
+    'msg' // message formatter
+  ];
+  for( name in methods ){
+    name = methods[name];
+    assert.equal(
+      typeof log[name], 'function',
+      'log#'+name+' should be a function'
+    );
+  }
+
+  // FINISHING
+  var sources = require('../api/sources')( module )
+  try {
+    assert.equal( sources.length, 7 );
+  } catch( error ){
+    console.error('WARN: This test should require 4 source files exactly');
+    console.error( 'source files required:\n  %s', sources.join('\n  ') );
+  }
 }
--- test/z-package.js
+++ test/z-package.js
@@ -1,12 +1,16 @@
-
-try {
-  var iai = require('..');
-} catch( err ){
-  console.error('requiring this package should not throw');
-  console.error( err.stack || err );
-  process.exit(1);
-}
-
 var assert = require('assert');
 
-assert.equal( iai.sources(module).length, 4, 'expected exactly 4 sources' );
+module.exports = TEST;
+require.main === module && TEST();
+
+function TEST(){
+  try {
+    var iai = require('..');
+  } catch( err ){
+    console.error('requiring this package should not throw');
+    console.error( err.stack || err );
+    process.exit(1);
+  }
+
+  assert.equal( iai.sources(module).length, 4, 'expected exactly 4 sources' );
+}
