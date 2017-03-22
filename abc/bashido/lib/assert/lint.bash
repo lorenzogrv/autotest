3cc139a2dce5201e45caae0359539f3621f9a26b
lorenzogrv <lorenzogrv@gmail.com>
2017-03-22 14:16:53 +0100

    add pending tests for abstract and autotest. Require an empty line after shebang on bash-head convention, better error messages on assert/lint

--- abc/bashido/lib/assert/lint.bash
+++ abc/bashido/lib/assert/lint.bash
@@ -7,13 +7,21 @@ assert_head_equal () {
 	local actual="$1" expect="$2"
 	assert_reg_exists "$actual"
 	assert_reg_exists "$expect"
-	diff_test <(head -n $(wc -l <"$expect") "$actual") "$expect"
+	diff_test <(head -n $(wc -l <"$expect") "$actual") "$expect" || {
+		verb "try the following commands to quick-fix the problem:"
+		verb "    mv $actual $actual.old"
+		verb "    cat $expect $actual.old > $actual"
+		verm "    rm $actual.old"
+	}
 }
 assert_tail_equal () {
 	local actual="$1" expect="$2"
 	assert_reg_exists "$actual"
 	assert_reg_exists "$expect"
-	diff_test <(tail -n $(wc -l <"$expect") "$actual") "$expect"
+	diff_test <(tail -n $(wc -l <"$expect") "$actual") "$expect" || {
+		verb "try the following commands to quick-fix the problem:"
+		verb "    cat $expect >> $actual"
+	}
 }
 ##
 # vim modeline
