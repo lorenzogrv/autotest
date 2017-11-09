120aab86230bbc486eb8715fd11e85e3b4c81345
lorenzogrv <lorenzogrv@gmail.com>
2017-11-09 05:31:28 +0100

    fix app/pingponggit status! :D

--- api/gui/BrowserWindowRemote.js
+++ api/gui/BrowserWindowRemote.js
@@ -4,7 +4,7 @@
 // @see https://github.com/electron/electron/blob/master/docs/api/browser-window.md
 
 var EventEmitter = require('events').EventEmitter;
-var iai = require('iai-abc');
+var oop = require('iai-oop');
 
 module.exports = BrowserWindowRemote;
 module.exports.prototype = Object.create(EventEmitter.prototype);
@@ -15,7 +15,7 @@ function BrowserWindowRemote(id, cp){
   // TODO here should be the instanceof check
   var instance = Object.create(this.prototype || module.exports.prototype);
   EventEmitter.call(instance);
-  iai.oop(instance)
+  oop(instance)
     .visible( 'id', id )
     .hidden( 'cp', cp )
     .visible( 'remoteCall', function( method, args ){
@@ -26,7 +26,7 @@ function BrowserWindowRemote(id, cp){
   return instance;
 }
 
-var exports = iai.oop( module.exports.prototype );
+var exports = oop( module.exports.prototype );
 var names = [
   "destroy", "close",
   "focus", "blur", "isFocused",
