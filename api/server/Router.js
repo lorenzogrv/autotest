a0db676e8a79cfeda2d928c788f445662c6612fa
lorenzogrv <lorenzogrv@gmail.com>
2018-02-01 02:37:40 +0100

    better loggin for Router

--- api/server/Router.js
+++ api/server/Router.js
@@ -6,14 +6,17 @@ log.level = iai.Log.VERB
 module.exports = Router
 
 function handle (req, res, next) {
-  log.verb('request %s', req.url)
+  res.on('finish', function () {
+    log.info('%s %s', res.statusCode, req.url)
+  })
 
   if (typeof this[req.url] === 'function') {
-    log.info('URI %s', req.url)
     return this[req.url](req, res)
   }
+
   // TODO foreach this[req.url] there may be regexps (string starts with ^)
   if (typeof next === 'function') {
+    // YAGNI?
     return next()
   }
   log.warn('404 %s', req.url)
