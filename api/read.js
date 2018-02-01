959411ed2643ec191307dac250a5827a0575f367
lorenzogrv <lorenzogrv@gmail.com>
2018-02-01 17:28:57 +0100

    fix iai.read: Handle data 'as-is', without decoding

--- api/read.js
+++ api/read.js
@@ -1,41 +1,60 @@
-var PassThrough = require('stream').PassThrough
-var StringDecoder = require('string_decoder').StringDecoder
-var Aslice = Array.prototype.slice
+const { PassThrough, Readable } = require('stream')
+const Aslice = Array.prototype.slice
 
-var abc = require('iai-abc')
-var log = abc.log
+const abc = require('iai-abc')
+const log = abc.log
 log.level = abc.Log.VERB
 
-
 /**
- * reads characters from any readable stream
- * if !n, it will read until \n (or opts.splitter) is found (and strip it)
+ * reads data from any readable stream
+ *    opts.stream (stream to read from) defaults to process.stdin
+ * if opts.n is given, it will read n bytes
+ * if opts.n is falsy, it will read until opts.splitter is found
+ *    opts.splitter defaults to 13 (enter key)
+ * TODO opts.strip => removes splitter from output
+ * if callback is given, reads only once
+ * returns ReadableStream
  */
 
-module.exports = read
+module.exports = readkeys
 
-function read (stream, opts, callback) {
+function readkeys (opts, callback) {
+  if (opts instanceof Readable) {
+    if (callback && typeof callback !== 'function') throw TypeError('Bad Call')
+    opts = { stream: opts }
+  }
   opts = opts || {}
-  // opts.t = opts.t || 2000; //TODO use timeout by default?
-  if (!opts.n) opts.splitter = opts.splitter || '\n'
-  log.info('READ STREAM', opts, 'Begin')
+  // don't use timeout by default
+  opts.t = opts.t || 0
+  // read -n strategy vs read-by-line strategy
+  if (!opts.n) {
+    opts.splitter = opts.splitter || 13
+    delete opts.n // ensure opts.n is undefined
+  }
 
-  stream = stream || process.stdin
-  // TODO do not force encoding if none given (so reading n-bytes)¿?
-  var decoder = new StringDecoder('utf8')
-  var result = ''
+  var stream = opts.stream || process.stdin
   var output = new PassThrough()
 
-  if (opts.n && stream === process.stdin) {
+  delete opts.stream
+  log.info('READ STREAM', opts, 'Begin')
+
+  if (stream === process.stdin) {
     stream.setRawMode(true)
     log.info('READ STDIN', opts, 'raw mode enabled')
     // TODO Move this behaviour to a iai.cleanup function
-    // taken from http://stackoverflow.com/a/21947851/1894803
-    process.on('exit', function (code) {
+    // basics from http://stackoverflow.com/a/21947851/1894803
+    var disableRaw = function () {
       stream.setRawMode(false)
       log.info('READ STDIN', opts, 'raw mode disabled')
-      output.end()
+    }
+    var onExit = function (code) {
+      disableRaw()
       code && log.error('READ STDIN', opts, 'main proces EXIT', code)
+    }
+    process.on('exit', onExit)
+    output.on('end', function () {
+      disableRaw()
+      process.removeListener('exit', onExit)
     })
   }
 
@@ -77,49 +96,47 @@ function read (stream, opts, callback) {
     log.info('READ STREAM', opts, 'End')
   })
 
+  var result = []
   return output
 
   function _read () {
     var chunk
     while ((chunk = stream.read(opts.n)) !== null) {
-      // TODO do not force encoding if none given (so reading n-bytes)¿?
-      var str = decoder.write(chunk)
-      log.debug('READ STREAM', opts, Aslice.call(chunk))
-
-      var remain = false
-      if (opts.n && opts.n >= (result.length + str.length)) {
-        // read -n strategy: read by length
-        var slice = str.slice(0, opts.n - result.length)
-        result += slice
-        remain = str.slice(slice.length)
-      } else if (~str.indexOf(opts.splitter)) {
-        // read "line" strategy: read until splitter is found
-        var split = str.split(opts.splitter)
-        result += split.shift()
-        remain = split.join(opts.splitter)
-      }
-      if (remain === false) {
-        result += str
-        continue
-      }
-      // unshift if we get too much
-      if (remain.length) {
-        log.verb('remain', remain)
-        stream.unshift(Buffer.from(remain, 'utf8'))
-      }
-      // this can be commented but it avoids uneccesary calls
-      // TODO decide if empty result pushes as splitter
-      if (!result) { log.debug('skiping empty splitter'); continue }
-      // check for ^C
-      if (~result.indexOf('\u0003')) {
+      // detect user interrupt via ^C
+      // TODO ^C detection should be deactivable via options
+      if (~chunk.indexOf(3)) {
         log.debug('READ', opts, 'Push null (^C)')
         output.push(null)
         return
       }
-      // finish
-      log.debug('READ', opts, 'Push', JSON.stringify(result))
-      output.push(result)
-      result = ''
+      var remain = false
+      // we may get too much, either reading by n-bytes or by line
+      if (opts.n && opts.n >= (result.length + chunk.length)) {
+        // read -n strategy: read by length (n bytes)
+        var slice = Aslice.call(chunk, 0, opts.n - result.length)
+        result = result.concat(slice)
+        remain = chunk.slice(slice.length)
+      } else if (~chunk.indexOf(opts.splitter)) {
+        // read "line" strategy: read until splitter is found
+        var split = Aslice.call(chunk, 0, chunk.indexOf(opts.splitter))
+        // TODO if splitter should be striped, don't concat it
+        result = result.concat(split, opts.splitter)
+        remain = chunk.slice(split.length + 1)
+      }
+      // unshift if we got too much
+      if (remain.length) {
+        log.verb('remain %j', Aslice.call(remain))
+        stream.unshift(remain)
+      }
+      if (remain === false) {
+        result = result.concat(Aslice.call(chunk))
+        log.verb('still reading %j', result)
+        continue
+      }
+      // finish reading this (push result)
+      log.debug('READ %j Push %j', opts, result)
+      output.push(Buffer.from(result))
+      result = []
     }
   }
 }
