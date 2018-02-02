cc693fd76589f8bfa5c622274fc0fe17809531db
lorenzogrv <lorenzogrv@gmail.com>
2018-02-02 06:31:37 +0100

    update app/pingpong: decouple server from run script

---
+++ app/pingpong/server.js
@@ -0,0 +1,34 @@
+var iai = require('iai')
+var log = iai.log
+var read = iai.readkeys
+
+log.level = iai.Log.VERB
+
+var server = module.exports = Object.create(iai.Server)
+
+var kb = null
+
+server
+  .on('request', require('./router'))
+  .on('ws:connection', function (ws) {
+    ws.send('echo connected to server!')
+    if (!kb) {
+      log.info('reading from stdin...')
+      kb = read()
+      .on('readable', function () {
+        server.broadcast(iai.f('stdin %s', this.read()))
+      })
+      .once('end', function () {
+        log.info('keyboard input end')
+	kb = null
+      })
+    }
+  })
+  .on('ws:message', function (ws, data) {
+    // TODO here it's supossed to delegate logic somewhere
+    log.warn('received message "%s"', data)
+    ws.send('echo received message: ' + data)
+  })
+  .on('ws:close', function (clients) {
+    if (kb && !clients.size) kb.end()
+  })
