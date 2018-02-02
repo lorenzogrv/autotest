839efdb91c970a8a19d511cad468bb0eb853bdb6
lorenzogrv <lorenzogrv@gmail.com>
2018-02-02 11:55:05 +0100

    update app/pingpong: better assests handling

--- app/pingpong/router.js
+++ app/pingpong/router.js
@@ -1,22 +1,24 @@
-var fs = require('fs')
 var iai = require('iai')
 var log = iai.log
 
 log.level = iai.Log.VERB
 
 // TODO this is the most simple posible view: serve a file "as-is"
+var fs = require('fs')
 var path = iai.path(__dirname)
 function Raw (file) {
   return (req, res) => fs.createReadStream(path.to(file)).pipe(res)
 }
 
-// TODO this should be an express.Router
-// so it does not suck as now :D
-module.exports = iai.Router({
-  '/': Raw('index.html'),
-  '/frontend.js': Raw('frontend.js'),
-  '/css/normalize.css': Raw('css/normalize.css'),
-  '/css/main.css': Raw('css/main.css'),
-  '/site.webmanifest.json': Raw('site.webmanifest.json'),
-  '/img/iai-icon.png': Raw('img/iai-icon.png')
-})
+var urls = {
+  '/': Raw('./www/index.html')
+}
+
+// automatically add handlers for assets
+require('child_process')
+  .spawnSync('find', ['./www', '-type', 'f'])
+  .stdout.toString('utf8').split('\n')
+  .filter((file) => file && !~file.indexOf('index.html'))
+  .forEach(function (file) { urls[file.slice(5)] = Raw(file) })
+
+module.exports = iai.Router(urls)
