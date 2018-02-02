c4e0c2b651ef010790c12624302a24ce93938967
lorenzogrv <lorenzogrv@gmail.com>
2018-02-02 02:53:21 +0100

    update server: refactor and better ws integration

--- api/server/server.js
+++ api/server/server.js
@@ -27,23 +27,29 @@ exports.listen = function () {
 // Never implement here details of what to do with messages
 // Nor send messages from here!!
 var wss = new WebSocketServer({ server: server })
-var clients = []
 wss.on('connection', function (ws) {
-  clients.push(ws)
-  log.verb('client connected, %d total', clients.length)
+  log.info('ws client connected, %d total', wss.clients.size)
 
+  ws.on('error', function (err) {
+    log.error('ws client error')
+    log.error(err.stack)
+  })
   ws.on('close', function (code, msg) {
     // close codes are defined on Web Socket RFC
     // see https://tools.ietf.org/html/rfc6455#section-7.4
-    log.verb('client closed (%d) "%s"', code, msg)
-    clients.splice(clients.indexOf(ws), 1)
+    log.info('ws client close (%d) "%s", %s left', code, msg, wss.clients.size)
+    server.emit('ws:close', wss.clients)
   })
   // re-emit connection on server
   server.emit('ws:connection', ws)
+  // re-emit message events from this websocket on server
+  ws.on('message', function (data) {
+    server.emit('ws:message', ws, data)
+  })
 })
 
 exports.broadcast = function (data, options, callback) {
-  log.debug('broadcast to %d clients', clients.length)
-  clients.forEach(function (ws) { ws.send(data, options, callback) })
-  return exports
+  log.debug('broadcast "%s" to %d clients', data, wss.clients.size)
+  for (let ws of wss.clients) ws.send(data, options, callback)
+  return this
 }
