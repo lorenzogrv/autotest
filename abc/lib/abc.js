4064dead0231280f93497ff1aa62b56deb7be270
lorenzogrv <lorenzogrv@gmail.com>
2018-02-05 22:53:15 +0100

    update iai-abc: export packdata, fix syntax to fullfill Standard Javascript code style

--- abc/lib/abc.js
+++ abc/lib/abc.js
@@ -3,43 +3,45 @@
  * License: MIT
  */
 
-var oop = require('iai-oop');
+var oop = require('iai-oop')
 
 //
 // This file is the main entry point.
 // Define here only the namespace aliases or accessors
 //
 
-var iai = module.exports;
+var iai = module.exports
 // make it prettier than [object Object]
-iai.toString = function(){ return '|iai-abc|'; };
+iai.toString = function () { return '|iai-abc|' }
 
 iai.f = require('util').format
 
 // from hereafter, use iai-oop to define exports
-var exports = oop( exports );
+var exports = oop(module.exports)
 
 // the "oop" namespace is reserved (not writable) for the OOP standard API
-exports.visible( 'oop', oop );
+exports.visible('oop', oop)
 // the "is" namespace is reserved (not writable) for the iai-is API
-exports.lazyload( 'is', require, 'iai-is' );
+exports.lazyload('is', require, 'iai-is')
 
 // the "path" namespace is reserved (not writable) for the path api
-exports.lazyLoad( 'path', require, './path' );
+exports.lazyLoad('path', require, './path')
 
 // the "Log" and "log" namespaces are reserved for accessing the logger api
-exports.lazyLoad( 'Log', require, './log' );
-exports.accessor( 'log', function getLog( ){
-  return iai.Log.constructor( getLog );
-});
+exports.lazyLoad('Log', require, './log')
+exports.accessor('log', function getLog () {
+  return iai.Log.constructor(getLog)
+})
 
 // "error" namespace is reserved for the custom error toolset
-exports.lazyLoad( 'error', require, './error' );
+exports.lazyLoad('error', require, './error')
 // "Error" namespace is reserved for CustomError constructor
-exports.lazyLoad( 'Error', function(){
-  var CustomError = require('./error/CustomError');
-  return CustomError.constructor.bind(CustomError);
-});
+exports.lazyLoad('Error', function () {
+  var CustomError = require('./error/CustomError')
+  return CustomError.constructor.bind(CustomError)
+})
+
+exports.lazyLoad('packdata', require, './tool/packdata')
 
 // setup process bindings
 // TODO this is too ugly (but actually process.js only binds events)
