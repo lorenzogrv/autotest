b88e1e9e710db0b1ca8657e428413c88ea67c836
lorenzogrv <lorenzogrv@gmail.com>
2018-03-04 22:46:09 +0100

    cleanup and refactor according to changes on answer.Router

--- app/pingpong/backend/router.js
+++ app/pingpong/backend/router.js
@@ -1,79 +1,47 @@
-const iai = require('iai')
+const iai = require('iai-api')
 const log = iai.log
 
 log.level = iai.Log.VERB
 
-const File = iai.answer.File
-
-const RootView = iai.View
-
-function View (view) {
-  return function (req, res) {
-    if (req.headers['x-requested-with'] === 'XMLHttpRequest') {
-      log.warn('AJAX REQUEST! %s', req.url)
-      log.warn('accept is %s', req.headers.accept)
-      // TODO this is actually a kind of answer.JSON, nothing to do with view
-      res.write(JSON.stringify(view))
-      return res.end()
-    }
-    log.warn('NON-AJAX REQUEST! %s', req.url)
-    var parts = req.url.split('/').slice(1)
-    var cumbs = []
-    var v = RootView
-    while (parts.length) {
-      v = v.create(parts.shift())
-      cumbs.push(v)
-      continue
-      // TODO this should be pushed instead
-      cumbs.push({
-        ref: null, // the parts.shift() thing
-        url: cumbs[cumbs.length - 1].url + '/' + 'name', // or "/"
-        obj: view
-      })
-    }
-    log.warn('here it should insert view on index and pipe it to response')
-    var $ = cheerio.load(index)
-    while (cumbs.length) {
-      cumbs.shift().inlay($)
-    }
-    res.write($.html())
-    res.end()
-  }
-}
-
 const path = iai.path(__dirname, '..')
-const fs = require('fs')
-const index = fs.readFileSync(path.to('www/index.html'), 'utf8')
-const cheerio = require('cheerio')
-
 const Section = iai.Section
 
-var home = Section.create('home')
-  .create('section-one')
-  .add('section-one-one')
-  .add('section-one-two')
-  .master
-  .add('section-two')
-  .create('section-three')
-  .add('section-three-one')
-  .add('section-three-two')
-  .master
-  .add('section-four')
-
-console.log(home.urls())
-
-var urls = {
-  '/terminal': View({
+var content = Section
+  .add('home', {
+    html: '<h1>This is the home screen</h1>'
+  })
+  .add('terminal', {
     html: { 'after': '<h1 id="stdin"></h1>' },
     css: [],
     js: []
-  }),
-  '/urls': View({
-    html: () => iai.f('%o', home.urls())
   })
+  .create('section-one', {
+    html: '<h1>This is the section one</h1>'
+  })
+  .add('section-one-one')
+  .add('section-one-two')
+  .master
+  .add('section-two', {
+    html: '<h1>This is the section two</h1>'
+  })
+  .create('section-three', {
+    html: '<h1>This is the section three</h1>'
+  })
+  .add('section-three-one')
+  .add('section-three-two')
+  .master
+  .add('section-four', {
+    html: '<h1>This is the section four</h1>'
+  })
+  .master
+
+var urls = {
+  '/unexistant-url': iai.answer.NotFound(),
+  '/urls': iai.answer.NotFound()
 }
 
 module.exports = iai.answer.Router({
   urls: urls,
+  menu: content,
   www: path.to('www')
 })
