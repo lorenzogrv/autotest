2796a992dff95465dfaae39f72c4ef74fab83486
lorenzogrv <lorenzogrv@gmail.com>
2018-03-04 22:37:48 +0100

    move window bindings from iai-api to iai-abc

--- api/browser.js
+++ api/browser.js
@@ -3,7 +3,6 @@
  * License: MIT
  */
 
-const { inspect } = require('util')
 const agnostic = require('./agnostic')
 
 // the iai object for browser inherits everything from iai agnostic object
@@ -14,48 +13,22 @@ const iai = module.exports = Object.create(agnostic)
  * Define here only the namespace aliases
  * Don't define lazy-load accesors as browserify will not understand them
  */
-//
 
 // there is no need to use the oop api to override things as needed
 
 // override path.__dirname so paths resolve as they should
 iai.path.__dirname = '/'
 
-// ugly but working hack to display an error on-screen
-iai.fatal = function fatal (msg, file, line, column, error) {
-  var title = error ? error.message || error : msg
-  var stack = error ? error.stack : Error('no stack trace').stack
-  document.body.innerHTML = '<h1>' + title + '</h1>'
-  document.body.innerHTML += '<pre>' + stack + '</pre>'
-  document.body.innerHTML += '<pre>' + inspect(error, { showHidden: true, depth: 4, showProxy: true }) + '</pre>'
-  // TODO don't prevent default handler to run until callsite is fixed
-  return false
-}
-
-// The hack above is meant for window.onerror
-if (window) window.onerror = iai.fatal
-
-// ugly but working hack to stop-and-debug at browser
-iai.debug = function debug (obj, title) {
-  document.body.innerHTML = '<pre>' + inspect(obj, {
-    showHidden: true, depth: 4, showProxy: true
-  }) + '</pre>'
-  window.onerror = function (msg) {
-    // TODO YAGNI if message does not match, use iai.fatal
-    document.body.innerHTML += '<p>Execution stoped intentionally</p>'
-    return true // let error stop execution
-  }
-  throw new Error('execution should stop')
-}
-
-// ugly hack to pipe logging to browser console
-iai.Log.output.on('data', (data) => console.log(data.toString('utf8')))
-
 // FROM HERE ONWARDS
 // will use iai-oop api to expose the augmented api
 //
 var exports = iai.oop(iai)
 
+// ugly but working hack to stop-and-debug at browser
+exports.visible('debug', require('iai-abc/lib/tool/debug'))
+
+// User interface implementation
+exports.visible('ui', require('./ui'))
+
 // View prototype implementation
 // TODO deprecate this
-exports.visible('View', require('./section'))
