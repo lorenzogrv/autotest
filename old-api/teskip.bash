4916ad2fc967844e594c94a8b12be733c40fb469
Lorenzo García Rivera <lorenzogrv@gmail.com>
2019-10-07 16:20:50 +0200

    code-style: expand tabs on old api source files

--- old-api/teskip.bash
+++ old-api/teskip.bash
@@ -2,7 +2,7 @@
 # #
 # Finishises a test, like `tested`, but without exiting when code was > 0
 teskip () {
-	echo "SKIP (code $?): $@"
+  echo "SKIP (code $?): $@"
 }
 
 ####
@@ -10,26 +10,27 @@ teskip () {
 # - $1: the function name, defaults to 'teskip'
 teskip--test () {
   local func=${1:-teskip}
-	(
-		source <(autotest) || exit
+  (
+    source <(autotest) || exit
 
-		test_code_0_and_continues () {
-			test $? -eq 0              ;tested "$cmd returns code 0"
-			[[ "$out" =~ continues$ ]] ;tested "$cmd does not call exit"
-		}
+    test_code_0_and_continues () {
+      test $? -eq 0              ;tested "$cmd returns code 0"
+      [[ "$out" =~ continues$ ]] ;tested "$cmd does not call exit"
+    }
 
-		cmd="'$func' running after a command that returns 0"
-		out="$( true; $func bar; echo continues )"
-		test_code_0_and_continues
+    cmd="'$func' running after a command that returns 0"
+    out="$( true; $func bar; echo continues )"
+    test_code_0_and_continues
 
-		cmd="'$func' running after a command that returns > 0"
-		out="$( false; $func foo; echo continues )"
-		test_code_0_and_continues
+    cmd="'$func' running after a command that returns > 0"
+    out="$( false; $func foo; echo continues )"
+    test_code_0_and_continues
 
-		test -n "$( $func foo )" ;tested "'$func' writes reports to stdout"
-	)
+    test -n "$( $func foo )" ;tested "'$func' writes reports to stdout"
+  )
 }
 
 ##
-# vim modeline
+# vim modeline (see vim +'help modeline')
+# /* vim: set expandtab: */
 # /* vim: set filetype=sh ts=2 shiftwidth=2: */
--- old-api/tested.bash
+++ old-api/tested.bash
@@ -5,10 +5,10 @@
 #     `(( $? )) && { echo FAIL; exit; } ; echo PASS`
 # and too much other refactors, just picked a explicit one
 tested () {
-	local code=$? ;	(( $code )) && {
-	  echo "FAIL $code $@"
-		exit $code
-	}
+  local code=$? ;  (( $code )) && {
+    echo "FAIL $code $@"
+    exit $code
+  }
   echo "PASS $@"
 }
 
@@ -17,28 +17,29 @@ tested () {
 # - $1: the function name, defaults to 'tested'
 tested--test () {
   local func=${1:-tested}
-	( # cannot use tested if this subshell fails
-		fail () { echo "$FUNCNAME: '$func' $@"; exit 1; }
+  ( # cannot use tested if this subshell fails
+    fail () { echo "$FUNCNAME: '$func' $@"; exit 1; }
 
-		out="$( true; $func bar; echo continues )"
-		test $? -eq 0 || fail "return code should be 0 if test passes"
-		[[ "$out" =~ continues$ ]] || fail "subshell should not have exit"
+    out="$( true; $func bar; echo continues )"
+    test $? -eq 0 || fail "return code should be 0 if test passes"
+    [[ "$out" =~ continues$ ]] || fail "subshell should not have exit"
 
-		out="$( false; $func foo; echo "continues" )"
-		test $? -ne 0 || fail "return code should not be 0 if test fails"
-		[[ "$out" =~ continues$ ]] && fail "subshell should have exited"
+    out="$( false; $func foo; echo "continues" )"
+    test $? -ne 0 || fail "return code should not be 0 if test fails"
+    [[ "$out" =~ continues$ ]] && fail "subshell should have exited"
 
-		exit 0
-	) || {
-	  echo "$FUNCNAME: '$func' does is not valid, see above for details"
-		exit 1
-	}
-	# now it's safe to report tests
-	$func "'$func' exits if last code failed, continues if passes"
-	test -n "$( $func foo )"
-	tested "'$func' writes reports to stdout"
+    exit 0
+  ) || {
+    echo "$FUNCNAME: '$func' does is not valid, see above for details"
+    exit 1
+  }
+  # now it's safe to report tests
+  $func "'$func' exits if last code failed, continues if passes"
+  test -n "$( $func foo )"
+  tested "'$func' writes reports to stdout"
 }
 
 ##
-# vim modeline
+# vim modeline (see vim +'help modeline')
+# /* vim: set expandtab: */
 # /* vim: set filetype=sh ts=2 shiftwidth=2: */
