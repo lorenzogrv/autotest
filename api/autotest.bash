2d3b4cfec980d8444b19e97beb2182587fc25031
Lorenzo García Rivera <lorenzogrv@gmail.com>
2019-10-08 05:09:54 +0200

    even more tests, leading new features

--- api/autotest.bash
+++ api/autotest.bash
@@ -1,4 +1,28 @@
 
+function autotest () {
+  # dumb function which bypasses api tests
+  :;
+}
+
+function load () { echo "not implemented"; exit 1; }
+
+function PASS () {
+  echo PASS "$@" 
+}
+
+function FAIL () {
+  # usage: FAIL [--next]? [code]? [arguments]{0,n}
+  # echoes specified [arguments] to standard output and exits with code 1
+  # --next option disables the exit behaviour
+  # TODO if [code] is known (see `autocode`), exits with that code instead
+  test "$1" = "--next" && {
+    shift; echo FAIL "$@"; return
+  }
+  echo FAIL "$@"
+  echo CODE 1 by $FUNCNAME \@${FUNCNAME[1]}
+  exit 1
+}
+
 ##
 # vim modeline (see vim +'help modeline')
 # /* vim: set expandtab: */
