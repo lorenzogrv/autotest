9ec3befaa6061efec6c685ff8f58ecd026944fa6
Lorenzo García Rivera <lorenzogrv@gmail.com>
2019-10-10 05:49:37 +0200

    add tests for check-command--outputs stdin feature, implement feature

--- api/check-command--outputs.bash
+++ api/check-command--outputs.bash
@@ -1,43 +1,65 @@
 # dirty, but working
 function check-command--outputs () {
-  if test "$1" == '-command'; then # no params received 
-    echo "$FUNCNAME: bad usage, insuficient parameters"
-    return 2
-  fi >&2
-
   local MODE EXPECT
-  while (($#)); do
-    case "$1" in
-      -command) shift; break ;;
-      #/*) MODE=diff shift ;;
-      something|anything) EXPECT=$1; shift; MODE='read -N 1' ;;
-      nothing) EXPECT=$1; shift; MODE='read -N 1 ; (($?))' ;;
-      string)
-        EXPECT="given string ($2)"
-        MODE="my-custom-diff - <(cat <<<\"$2\")"
-        shift 2
-        ;;
-      same-as-file)
-        EXPECT="contents of file $2"
-        MODE="my-custom-diff - \"$2\""
-        shift 2
-        ;;
-      *) echo "$FUNCNAME: invalid argv: $@" >&2; return 2 ;;
-    esac
-  done
-  # hereafter, argv should only contain the command line to be run
-  if "$@" 2>/dev/null | eval "$MODE" >&2
+  case "$1" in
+    -command)
+        echo "$FUNCNAME: bad usage, insuficient parameters" >&2
+        exit 2
+      ;;
+    something|anything)
+      EXPECT=$1; MODE='read -N 1'
+      ;;
+    nothing)
+      EXPECT=$1; MODE='read -N 1 ; (($?))'
+      ;;
+    string)
+      EXPECT="the string '$2'"
+      MODE="read out; test \"\$out\" = '$2' && ! read -N 1"
+      ;;
+    stdin)
+      EXPECT="same data read from stdin"
+      test -t 0 && FAIL "stdin is a terminal"
+      INPUT="$(cat)"
+      MODE="my-custom-diff - <(echo $(printf %q "$INPUT"))"
+      ;;
+    *) echo "$FUNCNAME: invalid argv: $@" >&2; return 2 ;;
+  esac
+
+  local output="" n=0
+  
+  diag-msg () {
+    printf '%2s | %s%s %s %s\n' $((++n)) "${1:-?}" "${4:->}" "$2" "${3:-<}"
+  }
+  diag () {
+    local l
+    while read l; do diag-msg $1 "$l" "$2" "$3"; unset -v l; done
+    test -n "$l" && diag-msg $1 "$l" "<(no line break present)" "$3"
+  } >&2
+
+  if output="$( "${AUTOCMD[@]}" 2> >(diag 2) | eval "$MODE" )"
   then # test passed
-    echo "PASS '$@' outputs $EXPECT"
+    echo "PASS$( printf " '%s'" "${AUTOCMD[@]}") outputs $EXPECT"
+    unset -f diag diag-msg
     return 0
   else
-    echo "FAIL '$@' should output $EXPECT, but it does not"
-    return 1
+    echo "FAIL '${AUTOCMD[@]}' should output $EXPECT, but it does not"
+    test -n "$INPUT" && diag 0 '>' '<' <<<"$INPUT"
+    case "$1" in something|anything) ;; *)
+      n=0
+      if test -n "$output"; then
+        diag 1 <<<"$output"
+      else
+        diag 1 <<<"$( "${AUTOCMD[@]}" 2>/dev/null )"
+      fi
+    esac
+    echo CODE 1
+    exit 1
   fi
 }
 
 function my-custom-diff () {
-  diff -y --left-column -W 80 --color=auto "$@"
+  diff -y --left-column -W 40 "$@"
+  #diff -y --left-column --suppress-common-lines -W 80 --color=auto "$@"
 }
 
 ##
