30316e00e1d6c8cf6259f94c16c638d13287ccdf
Lorenzo García Rivera <lorenzogrv@gmail.com>
2019-10-19 11:53:29 +0200

    add draft on how to use the autotest function to reuse tests at any layers. Still to research if this way to go is adequate

--- api/autotest.bash
+++ api/autotest.bash
@@ -3,28 +3,49 @@ function autotest () {
   # dumb function which bypasses api tests
   case "$1" in
     load)
-      source "$(autotest--root)/api/${2:?missing name}.bash"
+      test -n "$2" || EUSAGE "missing name" || exit
+      file="$(autotest--root)/0rigin/$2"
+      test -r "$file" \
+        || file="$(autotest--root)/api/$2.bash"
+      test -e "$file" || ENOENT "'$file' does not exist" || exit
+      set --
+      source "$file" \
+        || Error $? --trace "source $(printf %q "$2") raised $?"
+      verb "loaded $file"
+      return 0
       ;;
-    *) return 1
+    0|[A-Z])
+      file="$(autotest--root)/0rigin/$1"
+      shift
+      verb "will execute $file with argv='$@'"
+      ( exec -c -a "${file#$PWD/}" "$file" "$@" ) || exit $?
+      ;;
+    *) EUSAGE "missing implementation"
   esac
 }
 
-function PASS () {
-  echo PASS "$@" 
-}
+function Error () {
+  local trace code
+  test "$1" = "--trace" && { trace=1; shift; } || trace=0
+  code="${1:-3}"; shift # Default to 3: invalid exit status code
+  echo "|@${FUNCNAME[2]}: ${FUNCNAME[1]} Error:" "$@"
+  if (test $trace -eq 1 || verb "trace" )
+  then
+    n=0
+    while (( ++n < ${#BASH_SOURCE[@]}-1 )); do
+      read line funcname file <<<"$(caller "$n")"
+      test ${#funcname} -gt 6 && funcname=${funcname:0:4}..
+      #echo "@$funcname in $file +$line"
+      printf '|%7s: ' @$funcname
+      tail +$line "$file" | head -n 1 | xargs
+      printf '|%7s %4s %s\n' '>' +$line $file
+    done
+  fi
+  exit $code
+} >&2
 
-function FAIL () {
-  # usage: FAIL [--next]? [code]? [arguments]{0,n}
-  # echoes specified [arguments] to standard output and exits with code 1
-  # --next option disables the exit behaviour
-  # TODO if [code] is known (see `autocode`), exits with that code instead
-  test "$1" = "--next" && {
-    shift; echo FAIL "$@"; return
-  }
-  echo FAIL "$@"
-  echo CODE 1 by $FUNCNAME \@${FUNCNAME[1]}
-  exit 1
-}
+function EUSAGE () { Error 2 "$@"; }
+function ENOENT () { Error 127 "$@"; }
 
 ##
 # vim modeline (see vim +'help modeline')
